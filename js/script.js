21cd03ba8f774103c438bf17530097f0ac566586
Yutthana00 <arf101293@gmail.com>
2022-01-08 22:03:25 +0000

    DOM elements added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-// generator fuctions
+
+// DOM elements
+var resultEl = document.getElementById("result");
+var upperCaseEl = document.getElementById("upperCase");
+var lowerCaseEl = document.getElementById("lowerCase");
+var numbersEl = document.getElementById("numbers");
+var symbolsEl = document.getElementById("symbols");
+var generateEl = document.getElementById("generate");
+var lengthEl = document.getElementById("length");
+var clipboardEl = document.getElementById("clipboard");
+
+var randomFunc = {
+    symbols : getRandomSymbol,
+    number : getRandomNumber,
+    upper: getRandomUpper,
+    lower : getRandomLower,
+};
+
+
+// random generator fuctions (Number, UpperCase, LowerCase, Symbols)
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
