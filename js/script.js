528e42947a77c291d305868161fc385547f08a34
Yutthana00 <arf101293@gmail.com>
2022-01-08 21:40:24 +0000

    js, generate random letter

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// generator fuctions
+
+function getRandomLower() {
+
+return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// console.log(Math.floor(Math.random() * 26) + 97);
+console.log(getRandomLower());
